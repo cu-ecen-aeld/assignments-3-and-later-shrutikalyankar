bef620a90a044d26dc40b3877411fb11d16dd151
Shruti Sandeep Kalyankar <80039672+shrutikalyankar@users.noreply.github.com>
2026-02-08 15:43:04 -0700

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# Tester script for assignment 1 and assignment 2
-# Author: Siddhant Jajoo
+# Tester script for assignment 4
+# Runs correctly from /usr/bin with PATH-based executables
 
 set -e
 set -u
-
 
 OUTPUT_FILE=/tmp/assignment4-result.txt
 CONF_DIR=/etc/finder-app/conf
@@ -12,21 +11,20 @@ CONF_DIR=/etc/finder-app/conf
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
 
-if [ $# -lt 3 ]
-then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
-else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+USERNAME=$(cat ${CONF_DIR}/username.txt)
+ASSIGNMENT=$(cat ${CONF_DIR}/assignment.txt)
+
+if [ $# -ge 1 ]; then
+    NUMFILES=$1
+fi
+
+if [ $# -ge 2 ]; then
+    WRITESTR=$2
+fi
+
+if [ $# -ge 3 ]; then
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -35,43 +33,24 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=$(cat ${CONF_DIR}/assignment.txt)
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+if [ "${ASSIGNMENT}" != "assignment1" ]; then
+    mkdir -p "${WRITEDIR}"
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
-for i in $( seq 1 $NUMFILES)
-do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+for i in $(seq 1 ${NUMFILES}); do
+    writer "${WRITEDIR}/${USERNAME}${i}.txt" "${WRITESTR}"
 done
 
-finder.sh "${WRITEDIR}" "${WRITESTR" > "${OUTPUT_FILE}"
+# Run finder and capture output
+finder.sh "${WRITEDIR}" "${WRITESTR}" > "${OUTPUT_FILE}"
 
-# remove temporary directories
-rm -rf /tmp/aeld-data
-
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+# Verify output
+if grep -q "${MATCHSTR}" "${OUTPUT_FILE}"; then
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected '${MATCHSTR}'"
+    echo "actual output:"
+    cat "${OUTPUT_FILE}"
+    exit 1
 fi
